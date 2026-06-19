47be18ee66b8e6e451b1819598a928a7b5e998ec
HyunJuneKang <halfkang67@naver.com>
2026-06-19 09:03:00 +0900

    test A ConflictTest 작성

---
+++ board-practice/ConflictTest.java
@@ -0,0 +1,10 @@
+public class ConflictTest {
+
+    public String getBranchName() {
+        return "A 브랜치 코드입니다.";
+    }
+
+    public int calculate(int a, int b) {
+        return a + b;
+    }
+}
